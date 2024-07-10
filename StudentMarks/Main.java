9c1480156a30c558a5203d59cd5bfea8dd880984
Gimhan Lakshitha <73767965+GimhanL@users.noreply.github.com>
2024-07-10 18:35:24 +0800

    Adding marks and student user name on a loop

--- StudentMarks/Main.java
+++ StudentMarks/Main.java
@@ -6,16 +6,33 @@
  * @version (a version number or a date)
  */
 import java.util.Scanner;  // Import the Scanner class
-public class Main
-{
-  
-  public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter Student Marks");
 
-    String marks = myObj.nextLine();  // Read user input
-    System.out.println("Marks is: " + marks);  // Output user input
-  }
+class Main {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String[] userNames = new String[10];  // Create an array to store 10 usernames
+        String[] marks = new String[10];  // Create an array to store 10 usernames
+
+
+        // Loop to get 10 usernames
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter Student Name " + (i + 1) + ":");
+            userNames[i] = myObj.nextLine();  // Read user input and store it in the array
+             System.out.println("Enter marks of " +(i + 1)  + " student " + userNames[i]+ ":" );
+            marks[i] = myObj.nextLine();  // Read user input and store it in the array
+        }
+
+        // Close the Scanner
+        myObj.close();
+
+        // Print all usernames and marks
+        System.out.println("\nStudent Name and marks has been entered:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Student Name " + (i + 1) + ": " + userNames[i]);
+             System.out.println("marks " + (i + 1) + ": " + marks[i]);
+        }
+    }
 }
+
 
     
