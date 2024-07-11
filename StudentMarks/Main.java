6c2d3a4655e2cd8a0bd331665f18765a7e673e0a
Gimhan Lakshitha <73767965+GimhanL@users.noreply.github.com>
2024-07-11 15:35:10 +0800

    validation error fix

--- StudentMarks/Main.java
+++ StudentMarks/Main.java
@@ -11,25 +11,23 @@ class Main {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         String[] userNames = new String[10];  // Create an array to store 10 usernames
-        String[] marks = new String[10];  // Create an array to store 10 usernames
+        int[] marks = new int[10];  // Create an array to store 10 usernames
 
 
         // Loop to get 10 usernames
         for (int i = 0; i < 5; i++) {
-            System.out.println("Enter Student Name " + (i + 1) + ":");
-            userNames[i] = myObj.nextLine();  // Read user input and store it in the array
-             System.out.println("Enter marks of " +(i + 1)  + " student " + userNames[i]+ ":" );
-            marks[i] = myObj.nextLine();  // Read user input and store it in the array
-        String markInput = myObj.nextLine();  // Read user input as String
+             boolean validInput = false;  // Variable to check valid input
+            while (!validInput) {
+                System.out.println("Enter marks of " + userNames[i] + ":");
+                String markInput = myObj.nextLine();  // Read user input as String
                 try {
                     marks[i] = Integer.parseInt(markInput);  // Convert String to int
                     validInput = true;  // Input is valid, exit the loop
                 } catch (NumberFormatException e) {
-                    System.out.println("This is Not an number,Please enter a valid number.");
+                    System.out.println("This is not a number. Please enter a valid number.");
                 }
             }
         }
-
         // Close the Scanner
         myObj.close();
 
