79586b7b9d0ec53abcc756e7c6a7d0e1bd0749f1
Gimhan Lakshitha <73767965+GimhanL@users.noreply.github.com>
2024-07-31 01:04:46 +0800

    Creating main class
    Importing libraries and allocating arrays

---
+++ StudentMarks/StudentMarks.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class StudentMarks here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class StudentMarks
+{
+    public static void main(String[] args) {
+      // Create a Scanner object for user input
+        Scanner inputScanner = new Scanner(System.in);   
+        
+        // Adding 100 arrays to each variable 
+        String[] names = new String[100];
+        String[] studentIDs = new String[100];
+        float[] totalMarks = new float[100];
+        int[] marksCount = new int[100];
+  
+    }
+    
+}
