9b567ce0f7184a60541df3f0af862ba7b8c61bf5
Gimhan Lakshitha <73767965+GimhanL@users.noreply.github.com>
2024-08-03 16:33:44 +0800

    Calculating the average marks for the current student and assigning to the variable in  StudentsThreshold method

--- StudentMarks/StudentMarks.java
+++ StudentMarks/StudentMarks.java
@@ -206,7 +206,7 @@ private static double CalStandardDeviation(float[] marks, int[] marksCount, doub
     
     private static void StudentsThreshold(String[] names, String[] studentIDs, float[] totalMarks, int[] marksCount, float threshold) {
     for (int i = 0; i < names.length && names[i] != null; i++) {//adding a for loop to get the student details
-        
+        float averageMark = marksCount[i] > 0 ? totalMarks[i] / marksCount[i] : 0;  // Calculating the average marks for the current student
     
     }
 }
