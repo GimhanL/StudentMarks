87034a07a7592310c7b98d472f1a6bb299826b19
Gimhan Lakshitha <73767965+GimhanL@users.noreply.github.com>
2024-08-03 21:31:38 +0800

    Assigning the temporary variable to the next student

--- StudentMarks/StudentMarks.java
+++ StudentMarks/StudentMarks.java
@@ -250,8 +250,9 @@ private static void bubbleSort(Student[] students, int n) {//sorting students ba
     // Perform one pass of bubble sort
     for (int i = 0; i < n - 1; i++) {// creating a for loop to sort the data
           if (students[i].averageMarks < students[i + 1].averageMarks) { // swap the value if the current value is less than the next value
-              student = students[i];  //creating temporary variable to hold the current value of the student
-              students[i] = students[i + 1];     //swaping the stunt with the next student
+              Student temp= students[i];  //creating temporary variable to hold the current value of the student
+              students[i] = students[i + 1];//swapping the stunt with the next student
+               students[i + 1] = temp;// assigning the temporary variable to the next student
             }
     }
 }
