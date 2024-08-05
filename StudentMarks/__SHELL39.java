1c006a25e93ff9d948ea98143d1cf60c240a4b46
Gimhan Lakshitha <73767965+GimhanL@users.noreply.github.com>
2024-08-05 15:23:51 +0800

    Code Cleanup
    Removing unnecessary code part

---
+++ StudentMarks/__SHELL39.java
@@ -0,0 +1,8 @@
+
+public class __SHELL39 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+
+java.lang.String[] __bluej_param0 = { };
+StudentMarksAssignment2.main(__bluej_param0);
+
+}}
